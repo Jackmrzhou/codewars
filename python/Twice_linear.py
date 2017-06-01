37c9c39166f07e2810adbd008dffc5425604964a
Jackmrzhou <594780735@qq.com>
2017-06-01 17:10:00 +0800

    fix

--- python/Twice_linear.py
+++ python/Twice_linear.py
@@ -41,4 +41,18 @@ def dbl_linear(n):
 	#min_depth = int(math.log2(n) + 0.5) + 1
 
 	return s[n]
-print(dbl_linear(400))
+#always,my solution is worst
+#see other's great solution
+from collections import deque
+
+def dbl_linear(n):
+    h = 1; cnt = 0; q2, q3 = deque([]), deque([])
+    while True:
+        if (cnt >= n):
+            return h
+        q2.append(2 * h + 1)
+        q3.append(3 * h + 1)
+        h = min(q2[0], q3[0])
+        if h == q2[0]: h = q2.popleft()
+        if h == q3[0]: h = q3.popleft()
+        cnt += 1
